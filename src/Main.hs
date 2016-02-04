9d893784e8f3440288e607892800435c2b0a00ce
Scott Murphy <scottmurphy09@gmail.com>
2016-02-03 21:47:34 -0600

    Updated Report.hs with a csv implementation

--- src/Main.hs
+++ src/Main.hs
@@ -25,22 +25,28 @@ import           Onping.Tag.Report
 
 main :: IO ()
 main = do
-  (Right mongoConf) <- readDBConf "mongoDBConfig.yml"
-  (Right clientConf) <- readDataClientConf "onpingDataClient.yml"
-  (Right singleWellRocConfig) <- readSingleWellRocConfig "singleWellRocConfig.yml"
-  (Right singleWellMicrologixConfig) <- readSingleWellMicrologixConfig "singleWellMicrologixConfig.yml"
+  let printErrorEither = either fail  return
+  mongoConf <- printErrorEither =<< readDBConf "mongoConfig.yml"
+  clientConf <- printErrorEither =<< readDataClientConf "onpingDataClient.yml"
+  singleWellRocConfig <- printErrorEither =<< readSingleWellRocConfig "singleWellRocConfig.yml"
+  singleWellMicrologixConfig <- printErrorEither =<< readSingleWellMicrologixConfig "singleWellMicrologixConfig.yml"
   connectionPool <- buildConnectionPool mongoConf 3 256
+
   let plowStateStore = createHaxlStateStore $ PlowtechHaxlConf connectionPool clientConf singleWellRocConfig singleWellMicrologixConfig
   putStrLn "Welcome to Onping Tag Report Generator, Please choose an option"
-  putStrLn "1. Generate Tag Report for a Company."
+  putStrLn "1. Generate Html Tag Report for a Company."
+  putStrLn "2. Generate | S V Report for a Company"
   input <- getLine
   case input of
-    "1" -> do
-           putStrLn "Input a Company Name :"
-           companyname <- getLine
-           companySTemplate <- buildCompanyTemplate plowStateStore (pack companyname)
-           putStrLn companySTemplate
-           writeFile "onpingTagReport.md" companySTemplate
-           print ("Successfully Write to File: onpingTagReport.md"::String)
-           return ()
+    "1" -> handleInputAndRunAction plowStateStore buildCompanyTemplate "-onping-tag-report.html"
+    "2" -> handleInputAndRunAction plowStateStore buildCompanyTemplateCSV "-onping-tag-report.csv"
     _ -> print ("Not available option."::String)
+  where
+    handleInputAndRunAction plowStateStore action outputFile = do
+                                                               putStrLn "Input a Company Name :"
+                                                               companyname <- getLine
+                                                               companySTemplate <- action plowStateStore (pack companyname)
+                                                               putStrLn companySTemplate
+                                                               writeFile (companyname ++ outputFile) companySTemplate
+                                                               print ("Successfully Write to File"::String)
+                                                               return ()
--- src/Onping/Tag/Report.hs
+++ src/Onping/Tag/Report.hs
@@ -36,6 +36,8 @@ import qualified Plowtech.Request.Haxl.Site              as Haxl
 -- String Template
 import           Text.StringTemplate
 
+data ReportStyle = HTML | CSV
+
 -- Output Format
 -- # Onping Register Report
 
@@ -76,6 +78,7 @@ fetchOnpingTagCombineds plowStateStore locationId = do
          return otcEntities
            where
              altkeys = Haxl.ParameterLookupId [locationId] [] []
+
 
 
 buildOTCSTemplate :: OnpingTagCombined -> String
@@ -143,6 +146,62 @@ buildCompanyTemplate plowStateStore cName = do
                                      \# Onping Register Report \n\n\
                                      \## Company: $company_name$ \n\n\
                                      \$site_template$ \n") :: StringTemplate String
+             companySTemplate = setAttribute "company_name" cName .
+                                setAttribute "site_template" (unlines siteTemplates) $ baseTemplate
+         return $ render companySTemplate
+
+
+
+-- --------------------------------------------------
+
+buildOTCSTemplateCSV :: Text -> Text -> Text -> OnpingTagCombined -> String
+buildOTCSTemplateCSV cName sName locName otc = render otcSTemplate
+   where otcName = onpingTagCombinedDescription otc
+         otcSlaveParameterId = onpingTagCombinedSlave_parameter_id otc
+         otcParameterTagId = onpingTagCombinedParameter_tag_id otc
+         otcPID = onpingTagCombinedPid otc
+         baseTemplate = (newSTMP "$company_name$ | $site_name$ | $location_name$ | $pid$ | $tag_name$ | $slave_parameter_id$ | $parameter_tag_Id$ \n") :: StringTemplate String
+         otcSTemplate = setAttribute "tag_name" otcName .
+                        setAttribute "slave_parameter_id" otcSlaveParameterId .
+                        setAttribute "parameter_tag_Id" otcParameterTagId .
+                        setAttribute "company_name" cName.
+                        setAttribute "site_name" sName.
+                        setAttribute "location_name" locName.
+                        setAttribute "pid" otcPID $ baseTemplate
+buildTableSTemplateCSV :: Text -> Text -> Text -> [OnpingTagCombined] -> String
+buildTableSTemplateCSV cName sName lName otcList = render tableSTemplate
+   where otcSTemplates = unlines $ buildOTCSTemplateCSV cName sName lName <$> otcList
+         baseTemplate = (newSTMP "$otcSTemplate$ \n") :: StringTemplate String
+         tableSTemplate = setAttribute "otcSTemplate" otcSTemplates $ baseTemplate
+
+buildLocationTemplateCSV :: PlowStateStore -> Text -> Text -> Location -> IO String
+buildLocationTemplateCSV plowStateStore cName sName l = do
+       let locName = locationName l
+       otcList <- fetchOnpingTagCombineds plowStateStore (locationRefId l)
+       let tableTemplate = buildTableSTemplateCSV cName sName locName otcList
+           baseTemplate = (newSTMP "$tableTemplate$ \n") :: StringTemplate String
+           locationSTemplate = setAttribute "tableTemplate" tableTemplate $ baseTemplate
+       return $ render locationSTemplate
+
+buildSiteTemplateCSV :: PlowStateStore -> Text -> Site -> IO String
+buildSiteTemplateCSV plowStateStore cName s = do
+     let sName = siteName s
+     locList <- fetchLocations plowStateStore (siteSiteIdRef s)
+     locationTemplates <- traverse (buildLocationTemplateCSV plowStateStore cName sName) locList
+     let
+         baseTemplate = (newSTMP "$location_template$") :: StringTemplate String
+         siteSTemplate = setAttribute "location_template" (unlines locationTemplates) $ baseTemplate
+     return $ render siteSTemplate
+
+buildCompanyTemplateCSV :: PlowStateStore -> Text -> IO String
+buildCompanyTemplateCSV plowStateStore cName = do
+     companyList <- fetchCompanies plowStateStore cName
+     case companyList of
+       [] -> return "Error: There is no company records match the name given."
+       _ -> do
+         siteList <-fetchSites plowStateStore (companyCompanyIdRef . head $ companyList)
+         siteTemplates <- traverse (buildSiteTemplateCSV plowStateStore cName) siteList
+         let baseTemplate = (newSTMP "Company Name | Site Name | Location Name | Global Parameter Id | Tag Name | Slave Parameter Id | Parameter Tag ID \n $site_template$ \n") :: StringTemplate String
              companySTemplate = setAttribute "company_name" cName .
                                 setAttribute "site_template" (unlines siteTemplates) $ baseTemplate
          return $ render companySTemplate
